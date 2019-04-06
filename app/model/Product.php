4a430bce8bfcfa229f75383e33cb413b37eb89fd
MasudurRahman34 <mohammadmasud34@gmail.com>
2019-04-06 17:24:39 +0600

    product update and destroy

--- app/model/Product.php
+++ app/model/Product.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+	protected $fillable = [
+		'name','detail','price','stock','discount','user_id'
+	];
     public function reviews()
     {
     	return $this->hasMany(Reviews::class); 
